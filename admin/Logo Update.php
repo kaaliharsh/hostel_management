95cf87cfca73c16a0dce6cce64dfa72a02c4dc29
kaaliharsh <108045259+kaaliharsh@users.noreply.github.com>
2023-06-19 04:36:55 +0530

    Add files via upload

---
+++ admin/Logo Update.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Website</title>
+</head>
+<body>
+	<header>
+		<img src="current_logo.png" alt="My Website Logo">
+		<!-- Other header content here -->
+	</header>
+	<main>
+		<!-- Main content here -->
+	</main>
+	<footer>
+		<!-- Footer content here -->
+	</footer>
+</body>
+</html>
